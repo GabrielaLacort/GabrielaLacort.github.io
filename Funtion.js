30e11093030c56903d9e04c50f6c8c8a0f9fee91
GabrielaLacort <123596837+GabrielaLacort@users.noreply.github.com>
2023-01-25 23:26:31 -0300

    Add files via upload

---
+++ Funtion.js
@@ -0,0 +1,12 @@
+
+document.getElementById("boton").onclick = function () {
+    document.getElementById ("entrenemos").innerHTML= "Pilates Mat es una actividad que te permitira entrenar con tu propio peso corporal y a tu ritmo, es un entrenamiento conciente, en el que mente, cuerpo y espiritu trabajan en sinergia"
+}
+
+document.getElementById("streching").onclick = function () {
+    document.getElementById ("streching es").innerHTML= "El Streching o estiramientos es el puente entre la vida sedentaria y la activa, gracias a los estiramientos podemos mantener los músculos flexibles y prepararlos para el movimiento. Es fundamental en la preparación para cualquier actividad física, ya que evitara que te lesiones. Es ideal combinarlo con deportes o ejercicios de fuerza."
+}
+
+document.getElementById("funcional").onclick = function () {
+    document.getElementById ("funcional es").innerHTML= "El entrenamiento Funcional es una actividad que te permitira entrenar la fuerza de todo el cuerpo de manera integral con los elementos que tienes en casa, con ejercicios que iran desde lo simple a lo complejo, cada ejercicio será adaptado de acuerdo a tu experiencia en técnicas de entrenamiento y a posibles lesiones previas, para que puedas aprovechar cada entrenamiento.   "
+}
